f479d7997f16d1d453612acc39d242e22273ade5
Moxie Benavides <moxie@moxiege.com>
2024-07-03 03:39:23 -0500

    basic container setup

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              dotenv.enable = true;
+              packages = [];
+              processes.hello.exec = "hello";
+
+              languages.javascript = {
+                enable = true;
+                yarn.enable = true;
+              }
+
+              scripts = {
+                setup = ''
+                  yarn i
+                '';
+                clean = ''
+                  rm -rf node_modules 2> /dev/null
+                  rm -rf dist 2> /dev/null
+                  setup
+                '';
+                dev.exec = ''
+                  clean
+                  yarn run dev
+                '';
+                build.exec = ''
+                  clean
+                  yarn run build
+                '';
+                deploy.exec = ''
+                  clean
+                 yarn run deploy
+                '';
+              };
+            }
+          ];
+        };
+      });
+  };
+}
